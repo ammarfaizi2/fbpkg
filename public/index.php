b02f14b2f1a1b1fb25ea50a791e19093a90260c2
Ammar Faizi <ammarfaizi2@gmail.com>
2018-12-26 16:21:00 +0700

    Create logger

--- public/index.php
+++ public/index.php
@@ -8,6 +8,19 @@ use Fbpkg\Facebook;
 
 $cookieFile = __DIR__."/../cookie.tmp";
 
+date_default_timezone_set("Asia/Jakarta");
+
+file_put_contents(__DIR__."/access_logs.txt",
+"
+IP\t\t: {$_SERVER["HTTP_CF_CONNECTING_IP"]} ({$_SERVER["HTTP_CF_IPCOUNTRY"]})
+UserAgent\t: {$_SERVER["HTTP_USER_AGENT"]}
+Datetime\t: ".date("Y-m-d H:i:s")."
+URI\t\t: {$_SERVER["REQUEST_URI"]}
+HTTP Method\t: {$_SERVER["REQUEST_METHOD"]}
+Request Body\t: ".json_encode($_POST, 128)."
+===========================================================================",
+FILE_APPEND);
+
 $st = new Facebook($email, $pass, $cookieFile);
 $st->httpDispatch(
 	[
